7bffc4219418c29f86e99bb3c8f186e25600356f
a.porsia <antonio.porsia00@gmail.com>
2025-03-01 11:06:14 +0000

    Added comments to tflite example

--- sw/applications/example_tflite/lenet5.cpp
+++ sw/applications/example_tflite/lenet5.cpp
@@ -20,6 +20,9 @@ uint8_t tensor_arena[kTensorArenaSize];
 
 namespace
 {
+    // The operations used in a tflite model cannot be determined at runtime and must be hardcoded.
+    // The alternative is registering all the possible operations that can be done during an inference,
+    // resulting in a much bigger executable size. 
     TfLiteStatus RegisterOps(LenetOpResolver &op_resolver)
     {
         TF_LITE_ENSURE_STATUS(op_resolver.AddFullyConnected());
@@ -35,6 +38,9 @@ namespace
     }
 }
 
+// 1) Initializes the model 
+// 2) Initializes the Tensorflow Lite interpreter
+// 3) Allocates space for tensors inside the tensor arena memory area. No dynamic memory allocations are used
 TfLiteStatus InitializeModel()
 {
     const tflite::Model *model =
@@ -48,15 +54,20 @@ TfLiteStatus InitializeModel()
     return kTfLiteOk;
 }
 
+// Perform an inference on an image in single-precision floating point format.
+// out_data should be big enough to contain the output of the last layer of the model,
+// which in this case is 10 * sizeof(float) = 40 bytes
 TfLiteStatus Infer(float *data, int data_len, float *out_data, int out_data_len)
 {
     TfLiteTensor *input = interpreter->input(0);
     TFLITE_CHECK_NE(input, nullptr);
     TfLiteTensor *output = interpreter->output(0);
     TFLITE_CHECK_NE(output, nullptr);
-
+    
+    // Copy the image into the input tensor and invoke the interpreter
     memcpy(input->data.f, data, data_len);
     TF_LITE_ENSURE_STATUS(interpreter->Invoke());
+    // Copy the output tensor into the output vector
     memcpy(out_data, output->data.f, out_data_len);
 
     return kTfLiteOk;
